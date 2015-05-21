29d2af3471973bae8286a4487193bf989eef1beb
chris R <crs@masterbranch.de>
2015-05-21 12:22:18 +0200

    It's 2015; why are we using ColdFusion?!

--- add_proxy.sh
+++ add_proxy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-PROXY='http:\/\/141.39.208.110:3128'
 
 match='MAINTAINER'
 insert='ENV HTTP_PROXY='$PROXY' http_proxy='$PROXY' HTTPS_PROXY='$PROXY' https_proxy='$PROXY
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 ```
+# add proxy if needed:
+PROXY="YOUR_PROXY_ADDRESS" ./add_proxy.sh
+
 ./build.sh
+docker run -d -p 8888:8888 --name notebook notebookimg
 ```
